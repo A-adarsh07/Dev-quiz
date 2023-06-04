ff8983e224ba934ec3af5ba9b221c58c22ba7ee2
Adarsh Mishra <addy.mishra10@gmail.com>
2023-06-05 00:49:28 +0530

    added questions for the quiz

---
+++ general.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Which feature of OOP indicates code reusability?",
+    choice1: "Abstraction",
+    choice2: "Polymorphism",
+    choice3: "Encapsulation",
+    choice4: "Inheritance",
+    answer: 3
+},
+            {
+            question: " In multilevel inheritance, which is the most significant feature of OOP used?",
+    choice1: "Code efficiency",
+    choice2: "Code readability",
+    choice3: "Flexibility",
+    choice4: "Code reusability",
+    answer: 3
+},
+            {
+            question: " Which of the following is not true about polymorphism?",
+    choice1: "Helps in redefining the same functionality",
+    choice2: "Increases overhead of function definition always",
+    choice3: "It is feature of OOP",
+    choice4: " Ease in readability of program",
+    answer: 1
+},
+            {
+            question: "Which type of data can be stored in the database?",
+    choice1: " Image oriented data",
+    choice2: " Text, files containing data",
+    choice3: " Data in the form of audio or video",
+    choice4: "All of the above",
+    answer: 3
+},
+            {
+            question: "Which of the following is not a type of database?",
+    choice1: " Hierarchical",
+    choice2: " Network",
+    choice3: "Distributed",
+    choice4: " Decentralized",
+    answer: 3
+},
+            {
+            question: " The ability to query data, as well as insert, delete, and alter tuples, is offered by ____________?",
+    choice1: "TCL (Transaction Control Language)",
+    choice2: " DCL (Data Control Language)",
+    choice3: "DDL (Data Definition Langauge)",
+    choice4: "DML (Data Manipulation Langauge)",
+    answer: 3
+},
+            {
+               question: " ______________ is a set of one or more attributes taken collectively to uniquely identify a record.",
+    choice1: "Primary key",
+    choice2: "Foreign key",
+    choice3: "Super key",
+    choice4: "Candidate key",
+    answer: 2
+},{
+               question: " Which forms have a relation that contains information about a single entity?",
+    choice1: "4NF",
+    choice2: "2NF",
+    choice3: "5NF",
+    choice4: "3NF",
+    answer: 0
+},
+            {
+               question: " What is the main function of the command interpreter?",
+    choice1: " to provide the interface between the API and application program",
+    choice2: " to handle the files in the operating system",
+    choice3: "to get and execute the next user-specified command",
+    choice4: "none of the mentioned",
+    answer: 2
+},
+            {
+               question: " In a timeshare operating system, when the time slot assigned to a process is completed, the process switches from the current state to?",
+    choice1: "Suspended state",
+    choice2: "Terminated state",
+    choice3: "Ready state",
+    choice4: "Blocked state",
+    answer: 2
+}];
---
+++ jsquest.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+
+    choice1: "Position",
+    choice2: " Window",
+    choice3: "Standard",
+    choice4: "location",
+    answer: 1
+},
+{
+    question: "Which of the following scoping type does JavaScript use?",
+    choice1: "Sequential",
+    choice2: "Segmental",
+    choice3: "Lexical",
+    choice4: "Literal",
+    answer: 2
+},
+{
+    question: "Why JavaScript Engine is needed?",
+    choice1: " Both Compiling & Interpreting the JavaScript",
+    choice2: "Parsing the Javascript",
+    choice3: "Interpreting the javascript",
+    choice4: "Compiling the javascript",
+    answer: 2
+},
+{
+    question: " Why event handlers is needed in JS?",
+    choice1: " Allows JavaScript code to alter the behaviour of windows",
+    choice2: " Adds innerHTML page to the code",
+    choice3: "Change the server location",
+    choice4: " Performs handling of exceptions and occurrences",
+    answer: 0
+},
+{
+    question: " Which of the following is the property that is triggered in response to JS errors?",
+    choice1: "onclick",
+    choice2: "onerror",
+    choice3: "onmessage",
+    choice4: "onexception",
+    answer: 1
+},
+{
+    question: "Which function among the following lets to register a function to be invoked once?",
+    choice1: "setTimeout()",
+    choice2: "setTotaltime()",
+    choice3: "setInterval()",
+    choice4: "settime()",
+    answer: 0
+},
+{
+    question: " Which method receives the return value of setTimeout() to cancel future invocations?",
+    choice1: "clearTimeout()",
+    choice2: "clearInterval()",
+    choice3: "clearSchedule()",
+    choice4: "cancelInvocation()",
+    answer: 3
+}, {
+    question: "What are the three important manipulations done in a for loop on a loop variable?",
+    choice1: "Updation, Incrementation, Initialization",
+    choice2: " Initialization,Testing, Updation",
+    choice3: "Testing, Updation, Testing",
+    choice4: " Initialization,Testing, Incrementation",
+    answer: 1
+},
+{
+    question: " What will happen if the body of a for/in loop deletes a property that has not yet been enumerated?",
+    choice1: "The property will be stored in a cache",
+    choice2: "The loop will not run",
+    choice3: "That property will not be enumerated",
+    choice4: "The property will be enumerated",
+    answer: 2
+},
+{
+    question: " What must be done in order to implement Lexical Scoping?",
+    choice1: " Get the object",
+    choice2: "Dereference the current scope chain",
+    choice3: "Reference the current scope chain",
+    choice4: " Return the value",
+    answer: 2
+}];
